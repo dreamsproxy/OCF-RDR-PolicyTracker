082aceac943c8a903e73f2cd69e0d78c431ba9c7
dreamsproxy <changfengjui@gmail.com>
2023-01-29 12:33:02 +0800

    I have returned!

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import lxml
 from datetime import datetime
 
 def get_article_content(url, DEBUG):
+    #   Prepare tools and init lib functions
+    driver = webdriver.Firefox()
+    soup = BeautifulSoup(source_code, "lxml")
+
+    #   Prepare dataholders
+    full_webcontent = []
+    """
     headers = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET',
@@ -12,28 +19,39 @@ def get_article_content(url, DEBUG):
         'Access-Control-Max-Age': '3600',
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
     }
-    full_context = []
-
-    driver = webdriver.Firefox()
+    """
+    #   Preload webpage sourcecode
     driver.get(url)
-
     source_code = driver.page_source
 
 
-    #with open("./104.html", "r", encoding="utf-8") as f:
-    #    lastest_scan = f.read()
+    #   DEBUG/TEST CODE
+    #   Traverse and open up source code
+    #   Look for code tags of:
+    #       "span"
+    #       "style"
+    #       "script"
+    #       "a"
 
-    soup = BeautifulSoup(source_code, "lxml")
-
+    """
+    with open("./104.html", "r", encoding="utf-8") as f:
+        lastest_scan = f.read()
     for data in soup(["span", "style", "script", "a"]):
         data.decompose()
-    #print(soup)
-    source_log = soup.find_all(["h1", "h2", "h3", "href", "p", "meta"])
-    for context in source_log:
+    print(soup)
+    """
 
+    #   Search for all link and context tags, as well as meta tags
+    #       (comes in handy for decoding webcontent into correct charset)
+    source_log = soup.find_all(["h1", "h2", "h3", "href", "p", "meta"])
+
+    #   Iterate through individual lines of HTML code
+    for context in source_log:
+        #   If line in source code does not have any code:
         if len(context.contents) != 0:
+            #   Merge empty lines and 
             print(' '.join(soup.stripped_strings))
-            full_context.append(context.contents)
+            full_webcontent.append(context.contents)
 
         if DEBUG == True:
             source_encoding = str(context).split('"')[-2]
@@ -41,11 +59,11 @@ def get_article_content(url, DEBUG):
                 print(f"Page Encoding {source_encoding}")
 
     if DEBUG == True:
-        print(full_context)
+        print(full_webcontent)
 
-    for i, line in enumerate(full_context):
+    for i, line in enumerate(full_webcontent):
         try:
-            full_context[i] = line.decompose()
+            full_webcontent[i] = line.decompose()
             if DEBUG == True:
                 print("decomposed")
         except:
@@ -53,7 +71,7 @@ def get_article_content(url, DEBUG):
 
     log_time = datetime.now().strftime("%S-%M-%H-%d-%Y")
     with open(f"./104/{log_time}.txt", "w", encoding="utf-8") as save:
-        for line in full_context:
+        for line in full_webcontent:
             try:
                 save.write(f"{line[0]}\n")
             except:
